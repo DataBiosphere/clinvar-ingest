7ee248c52efe8028b10c1c696fba0d709db1f441
Raaid Arshad <rarshad@broadinstitute.org>
2020-03-11 10:07:57 -0400

    forgot to wrap the VERSION var properly

--- docker/kafka-python/build.sh
+++ docker/kafka-python/build.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 declare -r VERSION=1.0.0
 
-docker build -t us.gcr.io/broad-dsp-gcr-public/monster-kafka-python:VERSION .
-docker push us.gcr.io/broad-dsp-gcr-public/monster-kafka-python:VERSION
+docker build -t us.gcr.io/broad-dsp-gcr-public/monster-kafka-python:${VERSION} .
+docker push us.gcr.io/broad-dsp-gcr-public/monster-kafka-python:${VERSION}
