61b5d78c60d239a3ce3d5ff69be66835d319758e
Raaid Arshad <rarshad@broadinstitute.org>
2020-03-31 12:58:03 -0400

    add credentials.client_id to error message in check-existing-file.py

--- orchestration/scripts/check-existing-file.py
+++ orchestration/scripts/check-existing-file.py
@@ -9,18 +9,17 @@ authed_session = AuthorizedSession(credentials)
 base_url = os.environ["API_URL"]
 dataset_id = os.environ["DATASET_ID"]
 target_path = os.environ["TARGET_PATH"]
-headers = {"accept": "application/json"}
 
 
 def check_file_existence(target_path: str):
     response = authed_session.get(f"{base_url}/datasets/{dataset_id}/filesystem/objects",
-                                  params={"path": target_path},
-                                  headers=headers)
+                                  params={"path": target_path})
     if response.status_code == 200:
         return "true"
     elif response.status_code == 404:
         return "false"
     else:
-        raise HTTPError(f"Unexpected response, got code of: {response.status_code}")
+        raise HTTPError(f"Unexpected response, got code of: {response.status_code}"
+                        f"and the client_id is {credentials.client_id}")
 
 print(check_file_existence(target_path))
