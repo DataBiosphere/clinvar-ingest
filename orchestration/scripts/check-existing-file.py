dbb5859a3e742307cec33bb71621df7248256663
Raaid Arshad <rarshad@broadinstitute.org>
2020-04-08 12:43:28 -0400

    update python now

--- orchestration/scripts/check-existing-file.py
+++ orchestration/scripts/check-existing-file.py
@@ -1,17 +1,15 @@
-from google.auth import compute_engine
-import google.auth.transport.requests
+import google.auth
 from google.auth.transport.requests import AuthorizedSession
 from requests.exceptions import HTTPError
 import os
+
+credentials, project = google.auth.default(scopes=['openid', 'email', 'profile'])
 
 base_url = os.environ["API_URL"]
 dataset_id = os.environ["DATASET_ID"]
 target_path = os.environ["TARGET_PATH"]
 
-request = google.auth.transport.requests.Request()
-credentials = compute_engine.IDTokenCredentials(request, base_url, use_metadata_identity_endpoint=True)
 authed_session = AuthorizedSession(credentials)
-
 
 def check_file_existence(target_path: str):
     response = authed_session.get(f"{base_url}/api/repository/v1/datasets/{dataset_id}/filesystem/objects",
