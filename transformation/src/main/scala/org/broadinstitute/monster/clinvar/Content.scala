0845e98ec090929d0337c0158b088b9896366c51
Dan Moran <danmoran@broadinstitute.org>
2020-05-06 10:51:17 -0400

    Revert "Add a hack to sort content blocks."
    This reverts commit ce8c2bc2de2b9a56fc6372b7c7c3f8b897d3d7a8.

--- transformation/src/main/scala/org/broadinstitute/monster/clinvar/Content.scala
+++ transformation/src/main/scala/org/broadinstitute/monster/clinvar/Content.scala
@@ -1,13 +1,9 @@
 package org.broadinstitute.monster.clinvar
 
 import ujson.StringRenderer
-import upack.{Arr, Msg, Obj}
-
-import scala.collection.mutable
+import upack.Msg
 
 object Content {
-
-  private val renderer = StringRenderer()
 
   /**
     * Encode unmodeled data so it can be included in a string column.
@@ -18,27 +14,6 @@ object Content {
     if (msg.obj.isEmpty) {
       None
     } else {
-      Some(upack.transform(sortMsg(msg), renderer).toString)
+      Some(upack.transform(msg, StringRenderer()).toString)
     }
-
-  /**
-    * Hacky sort algorithm for upack Msgs.
-    *
-    * Used in an attempt to keep 'content' outputs consistent across
-    * releases.
-    */
-  private def sortMsg(msg: Msg): Msg = {
-    val sorted = mutable.LinkedHashMap.empty[Msg, Msg]
-    msg.obj.keysIterator.toList.sortBy(_.str).foreach { k =>
-      val original = msg.obj(k)
-      val subSorted = original match {
-        case o: Obj => sortMsg(o)
-        case Arr(buf) =>
-          Arr(buf.map(sortMsg).sortBy(upack.transform(_, renderer).toString))
-        case _ => original
-      }
-      sorted.put(k, subSorted)
-    }
-    Obj(sorted)
-  }
 }
