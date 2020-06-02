06fbbfbddf9311f28775303d959e4755e9c4b6a7
Dan Moran <danmoran@broadinstitute.org>
2020-06-02 14:44:16 -0400

    DSPDC-981 Add Kafka notification step.

--- docker/kafka-python/build.sh
+++ docker/kafka-python/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-declare -r VERSION=1.0.0
+declare -r VERSION=1.1.0
 
-docker build -t us.gcr.io/broad-dsp-gcr-public/monster-kafka-python:${VERSION} .
+docker build --build-arg KAFKA_CLIENT_VERSION=1.4.2 -t us.gcr.io/broad-dsp-gcr-public/monster-kafka-python:${VERSION} .
 docker push us.gcr.io/broad-dsp-gcr-public/monster-kafka-python:${VERSION}
