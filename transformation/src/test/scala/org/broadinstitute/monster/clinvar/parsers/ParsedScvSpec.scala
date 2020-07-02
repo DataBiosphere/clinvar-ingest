69ac0effc1add1a62c47656bc418b947b9dc6815
Dan Moran <danmoran@broadinstitute.org>
2020-07-02 15:46:05 -0400

    Add tests for trait-linking.

---
+++ transformation/src/test/scala/org/broadinstitute/monster/clinvar/parsers/ParsedScvSpec.scala
@@ -0,0 +1,32 @@
+package org.broadinstitute.monster.clinvar.parsers
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ParsedScvSpec extends AnyFlatSpec with Matchers {
+  behavior of "ParsedScv.Parser"
+
+  it should "parse a minimal SCV" in {
+    ???
+  }
+
+  it should "parse an SCV with multiple top-level variations" in {
+    ???
+  }
+
+  it should "parse an SCV with a tree of variations" in {
+    ???
+  }
+
+  it should "parse an SCV with extra observations" in {
+    ???
+  }
+
+  it should "cross-link to RCVs when there's only one option" in {
+    ???
+  }
+
+  it should "cross-link to RCVs when there are multiple options" in {
+    ???
+  }
+}
