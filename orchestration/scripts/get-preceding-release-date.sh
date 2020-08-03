ae6d99ed772cb7245d4e852fe282cb0d54fa7be8
Raaid Arshad <rarshad@broadinstitute.org>
2020-08-03 12:27:27 -0400

    in progress

---
+++ orchestration/scripts/get-preceding-release-date.sh
@@ -0,0 +1,15 @@
+set -euo pipefail
+
+declare -ra BQ_QUERY=(
+  bq
+  --location=US
+  --project_id=${PROJECT}
+  --synchronous_mode=true
+  --headless=true
+  --format=csv
+  query
+  --use_legacy_sql=false
+)
+declare -r TABLE="\`${JADE_PROJECT}.${JADE_DATASET}.processing_history\`"
+
+${BQ_QUERY[@]} "SELECT release_date FROM ${TABLE} WHERE release_date = DATE_SUB('${RELEASE_DATE}', INTERVAL 1 DAY) AND pipeline_version = '${VERSION}' LIMIT 1" | tail -n 1
