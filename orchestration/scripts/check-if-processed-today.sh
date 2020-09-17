321e329596db26b3e4e24c74353404ce91d0ee25
Raaid Arshad <rarshad@broadinstitute.org>
2020-09-17 17:11:50 -0400

    check to see if the processing history table has an entry from _today_; if it does, then we need to diff it and notify clingen. If it doesn't, we good. No, there will not be any instances where this lasts so long that the check should return true but the date is now the next date.

---
+++ orchestration/scripts/check-if-processed-today.sh
@@ -0,0 +1,15 @@
+set -euo pipefail
+
+declare -ra BQ_QUERY=(
+  bq
+  --location=US
+  --project_id=${PROJECT}
+  --synchronous_mode=true
+  --headless=true
+  --format=csv
+  query
+  --use_legacy_sql=false
+)
+declare -r TABLE="\`${JADE_PROJECT}.${JADE_DATASET}.processing_history\`"
+
+${BQ_QUERY[@]} "SELECT COUNT(1) FROM ${TABLE} WHERE processing_date = CURRENT_DATE() AND pipeline_version = '${VERSION}'" | tail -n 1
