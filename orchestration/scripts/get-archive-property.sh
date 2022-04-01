75804f387f8bf15bc45dbf5437ecc8d1e935d1ef
Drew Herbst <aherbst@broadinstitute.org>
2022-04-01 12:50:29 -0400

    Remove debug, remove coverage check

--- orchestration/scripts/get-archive-property.sh
+++ orchestration/scripts/get-archive-property.sh
@@ -12,5 +12,4 @@ declare -ra BQ_QUERY=(
 )
 declare -r TABLE="\`${JADE_PROJECT}.${JADE_DATASET}.xml_archive\`"
 
-
-${BQ_QUERY[@]} "SELECT ${PROPERTY} FROM ${TABLE} WHERE release_date = '${RELEASE_DATE}'"
+${BQ_QUERY[@]} "SELECT ${PROPERTY} FROM ${TABLE} WHERE release_date = '${RELEASE_DATE}'" | tail -n 1
